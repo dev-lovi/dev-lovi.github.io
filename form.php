77e3644035f65dcf4a33aed2a11ee33835d834a3
dev-lovi <santiagolovisotto@gmail.com>
2023-03-05 19:29:25 -0300

    php?? idk
    xD

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="form.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    
+
+<div class="container">
+    <form class="contact-form" action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Dev-Lovi">
+        <input type="text" name="mail" placeholder="lovicoding@gmail.com">
+        <input type="text" name="subject" placeholder="I would like a personal website">
+        <textarea name="message" placeholder="Message"></textarea>
+        <button type="submit" name="submit">SEND</button>
+</div>
+
+</form>
+
+
+</body>
+</html>
